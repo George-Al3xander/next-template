31e1c3dd2a9ee51f435f8960950ba03e85ff1607
George <george.valuiskyi@gmail.com>
2025-01-10 21:39:13 +0200

    refactor: fonts
    Move fonts to the ui/fonts directory. Add fonts to the tailwind config
    as variables.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
+import "@/styles/main.css";
 import { LayoutProps } from "@/types/app";
+import { geistMono, geistSans } from "@/ui/fonts";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -20,12 +10,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps) {
     return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                {children}
-            </body>
+        <html
+            lang="en"
+            className={`${geistSans.variable} ${geistMono.variable}`}
+        >
+            <body className="font-geist-mono">{children}</body>
         </html>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +8,10 @@ export default {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                "geist-sans": ["var(--font-geist-sans)", ...fontFamily.sans],
+                "geist-mono": ["var(--font-geist-mono)", ...fontFamily.mono],
+            },
             colors: {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
